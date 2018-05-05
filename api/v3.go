fdfb12e77aefa3569f7ba6a80e70325ae12370fe
Daniil Zinenko <daniil.zinenko@iqoption.com>
2018-05-05 23:49:27 +0300

    no useless methods

--- api/v3.go
+++ api/v3.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 
 	"github.com/bobrnor/MailHog-Server/config"
-	"github.com/bobrnor/MailHog-Server/monkey"
 	"github.com/bobrnor/MailHog-Server/websockets"
 	"github.com/bobrnor/storage"
 	"github.com/gorilla/mux"
@@ -38,15 +37,6 @@ func createAPIv3(conf *config.Config, r *pat.Router) *APIv3 {
 
 	r.Path(conf.WebPath + "/api/v3/{namespace}/search").Methods("GET").HandlerFunc(apiv3.search)
 	r.Path(conf.WebPath + "/api/v3/{namespace}/search").Methods("OPTIONS").HandlerFunc(apiv3.defaultOptions)
-
-	r.Path(conf.WebPath + "/api/v3/jim").Methods("GET").HandlerFunc(apiv3.jim)
-	r.Path(conf.WebPath + "/api/v3/jim").Methods("POST").HandlerFunc(apiv3.createJim)
-	r.Path(conf.WebPath + "/api/v3/jim").Methods("PUT").HandlerFunc(apiv3.updateJim)
-	r.Path(conf.WebPath + "/api/v3/jim").Methods("DELETE").HandlerFunc(apiv3.deleteJim)
-	r.Path(conf.WebPath + "/api/v3/jim").Methods("OPTIONS").HandlerFunc(apiv3.defaultOptions)
-
-	r.Path(conf.WebPath + "/api/v3/outgoing-smtp").Methods("GET").HandlerFunc(apiv3.listOutgoingSMTP)
-	r.Path(conf.WebPath + "/api/v3/outgoing-smtp").Methods("OPTIONS").HandlerFunc(apiv3.defaultOptions)
 
 	r.Path(conf.WebPath + "/api/v3/{namespace}/websocket").Methods("GET").HandlerFunc(apiv3.websocket)
 
@@ -165,98 +155,6 @@ func (apiv3 *APIv3) search(w http.ResponseWriter, req *http.Request) {
 	res.Total = total
 
 	b, _ := json.Marshal(res)
-	w.Header().Add("Content-Type", "application/json")
-	w.Write(b)
-}
-
-func (apiv3 *APIv3) jim(w http.ResponseWriter, req *http.Request) {
-	log.Println("[APIv3] GET /api/v3/jim")
-
-	apiv3.defaultOptions(w, req)
-
-	if apiv3.config.Monkey == nil {
-		w.WriteHeader(404)
-		return
-	}
-
-	b, _ := json.Marshal(apiv3.config.Monkey)
-	w.Header().Add("Content-Type", "application/json")
-	w.Write(b)
-}
-
-func (apiv3 *APIv3) deleteJim(w http.ResponseWriter, req *http.Request) {
-	log.Println("[APIv3] DELETE /api/v3/jim")
-
-	apiv3.defaultOptions(w, req)
-
-	if apiv3.config.Monkey == nil {
-		w.WriteHeader(404)
-		return
-	}
-
-	apiv3.config.Monkey = nil
-}
-
-func (apiv3 *APIv3) createJim(w http.ResponseWriter, req *http.Request) {
-	log.Println("[APIv3] POST /api/v3/jim")
-
-	apiv3.defaultOptions(w, req)
-
-	if apiv3.config.Monkey != nil {
-		w.WriteHeader(400)
-		return
-	}
-
-	apiv3.config.Monkey = config.Jim
-
-	// Try, but ignore errors
-	// Could be better (e.g., ok if no json, error if badly formed json)
-	// but this works for now
-	apiv3.newJimFromBody(w, req)
-
-	w.WriteHeader(201)
-}
-
-func (apiv3 *APIv3) newJimFromBody(w http.ResponseWriter, req *http.Request) error {
-	var jim monkey.Jim
-
-	dec := json.NewDecoder(req.Body)
-	err := dec.Decode(&jim)
-
-	if err != nil {
-		return err
-	}
-
-	jim.ConfigureFrom(config.Jim)
-
-	config.Jim = &jim
-	apiv3.config.Monkey = &jim
-
-	return nil
-}
-
-func (apiv3 *APIv3) updateJim(w http.ResponseWriter, req *http.Request) {
-	log.Println("[APIv3] PUT /api/v3/jim")
-
-	apiv3.defaultOptions(w, req)
-
-	if apiv3.config.Monkey == nil {
-		w.WriteHeader(404)
-		return
-	}
-
-	err := apiv3.newJimFromBody(w, req)
-	if err != nil {
-		w.WriteHeader(400)
-	}
-}
-
-func (apiv3 *APIv3) listOutgoingSMTP(w http.ResponseWriter, req *http.Request) {
-	log.Println("[APIv3] GET /api/v3/outgoing-smtp")
-
-	apiv3.defaultOptions(w, req)
-
-	b, _ := json.Marshal(apiv3.config.OutgoingSMTP)
 	w.Header().Add("Content-Type", "application/json")
 	w.Write(b)
 }
